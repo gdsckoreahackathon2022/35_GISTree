ee3f44fab9dc97d44ab4c9f9638b7d2455e48b93
dongmin Shin <newdm2000@gmail.com>
2022-02-05 10:43:03 +0900

    Issue(#25) add tensorflow file and ppt, modify directory name 2022-02-05 10:42

--- test/flutter_application_1/lib/location.dart
+++
@@ -1,9 +0,0 @@
-import 'package:location/location.dart';
-
-class UserLocation {
-  LocationData? currentlocation;
-  double? latitude;
-  double? longitude;
-
-  UserLocation({this.latitude, this.longitude, this.currentlocation});
-}
